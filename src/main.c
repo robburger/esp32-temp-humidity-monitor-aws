e6fa2801314cf9a4f00239cae5f5679232548e18
Rob Burger <rob.burgs@gmail.com>
2019-01-20 18:38:54 +0200

    Added basic terminal data-logging (#1)
    * Added basic logging of data
    
    * Added better RPC endpoint handling
    
    * Better consistent output of JSON

--- src/main.c
+++ src/main.c
@@ -16,7 +16,110 @@
  */
 
 #include "mgos.h"
+#include "mgos_dht.h"
+#include "mgos_rpc.h"
 
+static struct mgos_dht *s_dht = NULL;
+
+//------------------------------------------------------------------------------
+// MAIN TIMER
+//------------------------------------------------------------------------------
+static void dht_timer_cb(void *arg) {
+  float t = mgos_dht_get_temp(s_dht);
+  float h = mgos_dht_get_humidity(s_dht);
+
+  if (isnan(t) || isnan(h)) {
+    LOG(LL_INFO, ("Failed to read data from sensor"));
+    return;
+  }
+  LOG(LL_INFO, ("Temperature: %.2f *C Humidity: %.2f %%", t, h));
+  (void) arg;
+}
+
+//------------------------------------------------------------------------------
+// RPC: DHT.Temp.Read
+//------------------------------------------------------------------------------
+static void rpc_dht_temp_cb(struct mg_rpc_request_info *ri,
+                            void *cb_arg,
+                            struct mg_rpc_frame_info *fi,
+                            struct mg_str args) {
+  // Get the temperature
+  float t = mgos_dht_get_temp(cb_arg);
+
+  // If no/invalid data is returned, return
+  if (isnan(t)) {
+    LOG(LL_INFO, ("Failed to read data from sensor"));
+    return;
+  }
+
+  // Return JSON formatted data, rounded to 2 places
+  mg_rpc_send_responsef(ri, "{temp: %.2f}", t);
+  (void) fi;
+  (void) args;
+}
+
+//------------------------------------------------------------------------------
+// RPC: DHT.Humidity.Read
+//------------------------------------------------------------------------------
+static void rpc_dht_humidity_cb(struct mg_rpc_request_info *ri,
+                            void *cb_arg,
+                            struct mg_rpc_frame_info *fi,
+                            struct mg_str args) {
+  // Get the humidity
+  float h = mgos_dht_get_humidity(cb_arg);
+
+  // If no/invalid data is returned, return
+  if (isnan(h)) {
+    LOG(LL_INFO, ("Failed to read data from sensor"));
+    return;
+  }
+
+  // Return JSON formatted data, rounded to 2 places
+  mg_rpc_send_responsef(ri, "{humidity: %.2f}", h);
+  (void) fi;
+  (void) args;
+}
+
+//------------------------------------------------------------------------------
+// RPC: DHT.Stats.Read
+//------------------------------------------------------------------------------
+static void rpc_dht_stats_cb(struct mg_rpc_request_info *ri,
+                            void *cb_arg,
+                            struct mg_rpc_frame_info *fi,
+                            struct mg_str args) {
+  // Get the stats
+
+  float h = mgos_dht_get_humidity(cb_arg);
+  float t = mgos_dht_get_temp(cb_arg);
+
+  // If no/invalid data is returned, return
+  if (isnan(t) || isnan(h)) {
+    LOG(LL_INFO, ("Failed to read data from sensor"));
+    return;
+  }
+
+  // Return JSON formatted data, rounded to 2 places
+  mg_rpc_send_responsef(ri, "{temp: %.2f, humidity: %.2f}", t, h);
+  (void) fi;
+  (void) args;
+}
+
+//------------------------------------------------------------------------------
+// MAIN APP
+//------------------------------------------------------------------------------
 enum mgos_app_init_result mgos_app_init(void) {
+  // Initialise DHT22, fail and if unable
+  if ((s_dht = mgos_dht_create(mgos_sys_config_get_dht_pin(), DHT22)) == NULL)
+    return MGOS_APP_INIT_ERROR;
+  
+  // Add a timer and poll the DHT22
+  mgos_set_timer((mgos_sys_config_get_dht_freq() * 1000), true, dht_timer_cb, NULL);
+
+  // Register custom RPC endpoints
+  mg_rpc_add_handler(mgos_rpc_get_global(), "DHT.Temp.Read", "", rpc_dht_temp_cb, s_dht);
+  mg_rpc_add_handler(mgos_rpc_get_global(), "DHT.Humidity.Read", "", rpc_dht_humidity_cb, s_dht);
+  mg_rpc_add_handler(mgos_rpc_get_global(), "DHT.Stats.Read", "", rpc_dht_stats_cb, s_dht);
+
+  // App started sucessfully
   return MGOS_APP_INIT_SUCCESS;
 }
