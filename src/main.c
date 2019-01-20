db7875115997a1557528ef392ee5a756c3bc766c
Rob Burger <rob.burgs@gmail.com>
2019-01-20 17:25:19 +0200

    Added basic logging of data

--- src/main.c
+++ src/main.c
@@ -16,7 +16,46 @@
  */
 
 #include "mgos.h"
+#include "mgos_dht.h"
+#include "mgos_rpc.h"
+
+static struct mgos_dht *s_dht = NULL;
+
+static void dht_timer_cb(void *arg) {
+  float t = mgos_dht_get_temp(s_dht);
+  float h = mgos_dht_get_humidity(s_dht);
+
+  if (isnan(h) || isnan(t)) {
+    LOG(LL_INFO, ("Failed to read data from sensor"));
+    return;
+  }
+  LOG(LL_INFO, ("Temperature: %.2f *C Humidity: %.2f %%", t, h));
+  (void) arg;
+}
+
+static void rpc_dht_temp_cb(struct mg_rpc_request_info *ri,
+                            void *cb_arg,
+                            struct mg_rpc_frame_info *fi,
+                            struct mg_str args) {
+  mg_rpc_send_responsef(ri, "{value: %.2f}", mgos_dht_get_temp(cb_arg));
+  (void) fi;
+  (void) args;
+}
+
+static void rpc_dht_humidity_cb(struct mg_rpc_request_info *ri,
+                            void *cb_arg,
+                            struct mg_rpc_frame_info *fi,
+                            struct mg_str args) {
+  mg_rpc_send_responsef(ri, "{value: %.2f}", mgos_dht_get_humidity(cb_arg));
+  (void) fi;
+  (void) args;
+}
 
 enum mgos_app_init_result mgos_app_init(void) {
+  if ((s_dht = mgos_dht_create(mgos_sys_config_get_dht_pin(), DHT22)) == NULL)
+    return MGOS_APP_INIT_ERROR;
+  mgos_set_timer((mgos_sys_config_get_dht_freq() * 1000), true, dht_timer_cb, NULL);
+  mg_rpc_add_handler(mgos_rpc_get_global(), "Temp.Read", "", rpc_dht_temp_cb, s_dht);
+  mg_rpc_add_handler(mgos_rpc_get_global(), "Humidity.Read", "", rpc_dht_humidity_cb, s_dht);
   return MGOS_APP_INIT_SUCCESS;
 }
